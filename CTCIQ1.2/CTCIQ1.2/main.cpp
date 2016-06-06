22d667ed2d86029ca4ca63dba398d6f041702e0b
Brian <brawnquake@gmail.com>
2016-06-05 22:37:24 -0700

    Question 1.2 started

---
+++ CTCIQ1.2/CTCIQ1.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CTCIQ1.2
+//
+//  Created by Brian Corson on 6/5/16.
+//  Copyright © 2016 Brian Corson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
