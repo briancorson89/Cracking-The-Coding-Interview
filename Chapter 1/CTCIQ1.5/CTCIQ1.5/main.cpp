65082767f6554731f7ccf5253f098ab07a9d400a
Brian <brawnquake@gmail.com>
2016-06-07 23:21:31 -0700

    started 1.5 logic correct mods to come later

---
+++ Chapter 1/CTCIQ1.5/CTCIQ1.5/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  CTCIQ1.5
+//
+//  Created by Brian Corson on 6/7/16.
+//  Copyright © 2016 Brian Corson. All rights reserved.
+//
+//  Implement a method to perform basic string compression using the counts of repeated character
+//  aabcccccaaa -> a2b1c5a3
+#include <iostream>
+
+using namespace std;
+
+string strCompression(string str){
+    if(str.length() <= 2){
+        return str;
+    }
+    char currentChar = str[0];
+    int charCount = 1;
+    char newStr[str.length()];
+    int newStrIter = 0;
+    for(int i = 0; i < str.length(); i++){
+        if(str[i + 1] != currentChar){
+            newStr[newStrIter++] = currentChar;
+            newStr[newStrIter++] = charCount + 48;
+            currentChar = str[i + 1];
+            charCount = 1;
+        }
+        else{
+            charCount++;
+        }
+    }
+    return newStr;
+}
+int main(int argc, const char * argv[]) {
+    cout << strCompression("aabcccccaaa") << endl;
+    return 0;
+}
