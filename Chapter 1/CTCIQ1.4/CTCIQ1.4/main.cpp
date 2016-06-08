774807c44ec5f181bd372cb6409aace6e2257529
Brian <brawnquake@gmail.com>
2016-06-07 22:06:07 -0700

    Question 1.4 completed

---
+++ Chapter 1/CTCIQ1.4/CTCIQ1.4/main.cpp
@@ -0,0 +1,46 @@
+//
+//  main.cpp
+//  CTCIQ1.4
+//
+//  Created by Brian Corson on 6/7/16.
+//  Copyright © 2016 Brian Corson. All rights reserved.
+//
+//  1.4 - Write a function to replace all spaces in a string with '%20'
+
+#include <iostream>
+
+using namespace std;
+
+string replaceSpaces(string str, int strLength){
+    int spacesCounter = 0;//iterate through string and count spaces
+    for(int i = 0; i < strLength;i++){
+        if(str[i] == ' '){
+            spacesCounter++;
+        }
+    }
+    spacesCounter *= 2;//for new string length we need to add 2 spots for every space
+    strLength += spacesCounter;
+    char newStr[strLength];//initialize char array for new string with necessary space allocated
+    int newStrIter = 0;
+    for(int i = 0; i < strLength; i++){
+        if(str[i] == ' '){
+            newStr[newStrIter++] = '%';
+            newStr[newStrIter++] = '2';
+            newStr[newStrIter++] = '0';
+        }
+        else{
+            newStr[newStrIter++] = str[i];
+        }
+    }
+    return newStr;
+}
+int main(int argc, const char * argv[]) {
+    cout << replaceSpaces("Mr. John Smith", 13) << endl;//expect: Mr.%20John%20Smith
+    cout << replaceSpaces("But we had one foot on the gas and one foot in the grave", 56) << endl;
+    //expect:But%20we%20had%20one%20foot%20on%20the%20gas%20and%20one%20foot%20in%20the%20grave
+    return 0;
+}
+/* Output:
+ Mr.%20John%20Smith
+ But%20we%20had%20one%20foot%20on%20the%20gas%20and%20one%20foot%20in%20the%20grave
+*/
