a0aa7c4b380571d62e741038a13d3ff5f2db555b
Brian <brawnquake@gmail.com>
2016-06-07 23:47:59 -0700

    finished 1.5

--- Chapter 1/CTCIQ1.5/CTCIQ1.5/main.cpp
+++ Chapter 1/CTCIQ1.5/CTCIQ1.5/main.cpp
@@ -6,14 +6,15 @@
 //  Copyright © 2016 Brian Corson. All rights reserved.
 //
 //  Implement a method to perform basic string compression using the counts of repeated character
+//  If the compressed string is larger than the old one, return the old one
 //  aabcccccaaa -> a2b1c5a3
 #include <iostream>
 
 using namespace std;
 
 string strCompression(string str){
-    if(str.length() <= 2){
-        return str;
+    if(str.length() <= 2){//when string is 2 letters or smaller, avoid stepping in algorithm
+        return str;//old string will be smaller
     }
     char currentChar = str[0];
     int charCount = 1;
@@ -21,10 +22,11 @@ string strCompression(string str){
     int newStrIter = 0;
     for(int i = 0; i < str.length(); i++){
         if(str[i + 1] != currentChar){
-            newStr[newStrIter++] = currentChar;
+            newStr[newStrIter++] = currentChar;//store char in new string, increment the iterator
             newStr[newStrIter++] = charCount + 48;
-            currentChar = str[i + 1];
-            charCount = 1;
+            // convert char to correct ascii figure in dec and store in new string and increment iterator
+            currentChar = str[i + 1];//update current char with next char
+            charCount = 1;//update char counter
         }
         else{
             charCount++;
@@ -36,7 +38,11 @@ string strCompression(string str){
     return newStr;
 }
 int main(int argc, const char * argv[]) {
-    cout << strCompression("aabcccccaaa") << endl;
-    cout << strCompression("abc") << endl;
+    cout << strCompression("aabcccccaaa") << endl;//expect: a2b1c5a3
+    cout << strCompression("ab") << endl; //expect:ab
     return 0;
 }
+/* Output:
+ a2b1c5a3
+ ab
+*/
