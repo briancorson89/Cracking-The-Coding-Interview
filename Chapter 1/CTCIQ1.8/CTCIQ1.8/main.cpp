8352f3a80fb510935230d401349382eecdee4d55
Brian <brawnquake@gmail.com>
2016-06-23 20:32:28 -0700

    minor changes to 1.8

--- Chapter 1/CTCIQ1.8/CTCIQ1.8/main.cpp
+++ Chapter 1/CTCIQ1.8/CTCIQ1.8/main.cpp
@@ -5,7 +5,9 @@
 //  Created by Brian Corson on 6/23/16.
 //  Copyright © 2016 Brian Corson. All rights reserved.
 //
-//  1.8 -- Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring(e.g. "waterbottle is a rotation of "erbottlewat
+//  1.8 -- Assume you have a method isSubstring which checks if one word is a substring of another. Given
+//  two strings s1 and s2, write code to check if s2 is a rotation of s1 using only one call
+//  to isSubstring(e.g. "waterbottle is a rotation of "erbottlewat")
 #include <iostream>
 #include <string>
 
