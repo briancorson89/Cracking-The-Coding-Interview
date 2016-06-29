277891421198c431165d75ab382eeb307f28e2fb
Brian <brawnquake@gmail.com>
2016-06-28 21:39:37 -0700

    added question 2.2 into LinkedList implementation

--- Chapter 2/CTCILinkedListImplementation/CTCILinkedListImplementation/main.cpp
+++ Chapter 2/CTCILinkedListImplementation/CTCILinkedListImplementation/main.cpp
@@ -24,10 +24,13 @@ public:
     void insert(int value);//function to create a node
     void display();//display the list
     void removeDuplicates(Node* head);//Question 2.1
-    
+    void kthToLastDisplay(Node* head, int index);//Question 2.2
+private:
+    int nodeCounter;//for question 2.2
 };
 
 LinkedList::LinkedList(){
+    nodeCounter = 0;
     this->head = nullptr;//construct an empty list
 }
 
@@ -49,19 +52,21 @@ void LinkedList::insert(int value){
         tail = head;//tail and head points to only node
         head->next = nullptr;//next is null pointer
         head->data = value;//data component in node struct given value
+        nodeCounter++;
     }
     else{
         tail->next = new Node();//create a node
         tail = tail->next;//tail points to newly created node
         tail->data = value;//give node value
         tail->next = nullptr;//create null pointer
+        nodeCounter++;
     }
 }
 
 void LinkedList::display(){
     Node* temp;
     if (head == NULL){//display for an empty list
-        cout << "This is an Empty List" << endl;
+        cout << "This is an Empty List. Node Count:" << nodeCounter << endl;
         return;
     }
     temp = head;//temp emcompasses head node
@@ -69,7 +74,7 @@ void LinkedList::display(){
         cout << temp->data << "->";//display data in temp node
         temp = temp->next;//temp emcompasses next node
     }
-    cout <<"null" << endl;//print null node
+    cout <<"null. Node count:" << nodeCounter << endl;//print null node
 }
 
 //  Question 2.1 -- Write code to remove duplicates from an unsorted linked list
@@ -89,6 +94,7 @@ void LinkedList::removeDuplicates(Node* head){
                 Node* temp = runner->next;//temp emcompases duplicate node
                 runner->next = runner->next->next;//temp is no longer pointed to in the lsit
                 delete temp;//delete node
+                nodeCounter--;
             }
             else{
                 runner = runner->next;//move to next node if not the same
@@ -97,23 +103,49 @@ void LinkedList::removeDuplicates(Node* head){
         current = current->next;//current moves to the next node
     }
 }
-
+//Question 2.2 -- Implement an argument to find the kth to last element of the list
+void LinkedList::kthToLastDisplay(Node* head, int index){
+    Node* current = head;
+    if(index > nodeCounter || nodeCounter == 0){
+        cout << "List is not long enough for function to succeed" << endl;
+        return;
+    }
+    int count = 1;
+    cout << "KthToLastDisplay(" << index << "):";
+    while(current){
+        if(count >= index){
+            cout << current->data << "->";
+        }
+        current = current->next;
+        count++;
+    }
+    cout << "null. Node Count:" << nodeCounter - index << endl;
+}
 int main(int argc, const char * argv[]) {
     LinkedList* myList = new LinkedList();
-    myList->display();//Expect: This is an Empty List
+    myList->display();//Expect: This is an Empty List. Node Count:0
+    myList->kthToLastDisplay(myList->head, 1);//Expect: List is not long enough for function to succeed
     myList->insert(1);
     myList->insert(1);
     myList->insert(2);
     myList->insert(1);
-    myList->display();//Expect: 1->1->2->1->null
+    myList->insert(12);
+    myList->insert(11);
+    myList->insert(100);
+    myList->insert(2);
+    myList->display();//1->1->2->1->12->11->100->2->null. Node count:8
+    myList->kthToLastDisplay(myList->head, 5);//Expect: KthToLastDisplay(5):12->11->100->2->null. Node Count:3
     myList->removeDuplicates(myList->head);
-    myList->display();//Expect: 1->2->null
+    myList->display();//Expect: 1->2->12->11->100->null. Node count:5
+    delete myList;
     return 0;
 }
 /* Output:
- This is an Empty List
- 1->1->2->1->null
- 1->2->null
+ This is an Empty List. Node Count:0
+ List is not long enough for function to succeed
+ 1->1->2->1->12->11->100->2->null. Node count:8
+ KthToLastDisplay(5):12->11->100->2->null. Node Count:3
+ 1->2->12->11->100->null. Node count:5
  */
 
 
